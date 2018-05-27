3db46a9859be37268dda04bd3897d234c85c4093
Peter <peterrolanddias@gmail.com>
2018-05-27 23:36:21 +0800

    Add comments to the file

--- js/robot_simulator.js
+++ js/robot_simulator.js
@@ -7,10 +7,12 @@ var MoveRobot = {
             var commands = $(this).serializeArray()[0].value.toLowerCase().split(/\b\s*,?\s*/);
             var command = commands.shift();
             if (command.length) { //check whether a command is specified
+                //Check to confirm no other command is executed before the robot is on the table
                 if (command != 'place' && !MoveRobot.isRobotOnTable()){
                     MoveRobot.error('Please place robot on table first.');
                     return;
                 }
+                //Main command switch
                 switch (command) {
                     case 'place':
                         if (commands.length == 3) {
@@ -39,16 +41,21 @@ var MoveRobot = {
             MoveRobot.error("Invalid command");
         });
     },
+    //Convience function to return check if the robot is on the table
     'isRobotOnTable': function(){
         var position = $('#robot').position();
         return MoveRobot.validMove(position.left, position.top);
     },
+    //Generic error handling/display function
     'error': function(message){
+        //Uses bootstrap alerts
         $('#error-message').html('<div class="alert alert-danger alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+message+'</div>');
     },
+    //Test funtion to confirm convertCoordinatesToAbsolute and convertAbsoluteToCoordinates are working correctly
     'testConversions': function (x,y){
         var absolute = MoveRobot.convertCoordinatesToAbsolute(x,y);
         var coordintes = MoveRobot.convertAbsoluteToCoordinates(absolute[0], absolute[1]);
+        //Since the coordinates are mathematically calculated we need to round them up
         if (Math.round(coordintes[0]) == x && Math.round(coordintes[1]) == y) {
             alert('values match');
             return true;
@@ -57,6 +64,7 @@ var MoveRobot = {
         alert('Values do not match ');
         return false;
     },
+    //Validate whether the position provided falls withing the boundaries of the table
     'validMove': function(x, y) {
         var table = $('#table');
         var position = table.position();
@@ -79,6 +87,7 @@ var MoveRobot = {
                 return false;
         }
     },
+    //Rotate robot left and store direction
     'rotateLeft': function(){
         //Other ways we could do this is :
         // -> use CSS 'rotate' to visually show the change in direction, we still need to store it for move
@@ -104,6 +113,7 @@ var MoveRobot = {
         }
         MoveRobot.setDirection(newDirection);
     },
+    //Rotate robot right and store direction
     'rotateRight': function(){
         //Other ways we could do this is :
         // -> use CSS 'rotate' to visually show the change in direction, we still need to store it for move
@@ -130,32 +140,35 @@ var MoveRobot = {
 
         MoveRobot.setDirection(newDirection);
     },
-
+    //Report handling function
     'report': function () {
         var position = $('#robot').position();
         var coords = MoveRobot.convertAbsoluteToCoordinates(position.left, position.top);
         alert('Current Position is '+coords[0]+','+coords[1]+' '+$('#robot').attr('direction'));
     },
-
+    //The height of the table is adjusted to accomodate the image dimensions
     'getAdjustedHeight': function(){
         return $('#table').height() - 36;
     },
-
+    //Get each individual step
     'getIndividualUnit': function(){
         return (MoveRobot.getAdjustedHeight() / 5);
     },
-
+    //Converts coordinates in the form of 0,1 to actual position values
     'convertCoordinatesToAbsolute': function(x, y) {
         var table = $('#table');
         var position = table.position();
 
         //Assuming a standard of 5x5 units
         var individualUnit = MoveRobot.getIndividualUnit();
+        //Since .position() gives us the top left corner of an element we need to do some math to get the 0,0
+        // position which according to this task is the south west most corner
         var absX = Math.abs(individualUnit * x + position.left);
         var absY = Math.abs(individualUnit * y - (position.top + MoveRobot.getAdjustedHeight()));
 
         return [absX, absY];
     },
+    //Converts absolute position to coordinates: Does inverse of above
     'convertAbsoluteToCoordinates': function(x, y) {
         var position = $('#table').position();
         var individualUnit = MoveRobot.getIndividualUnit();
@@ -164,11 +177,11 @@ var MoveRobot = {
 
         return [Math.round(coordX), Math.round(coordY)];
     },
-
+    //Move the robot one step in the direction previously stored
     'move': function() {
         var robot = $('#robot');
         var currentPos = robot.position();
-        //We don't have to convert to coordinates. We could just increment my 'individualUnit'
+        //We don't have to convert to coordinates. We could just increment by 'individualUnit'
         var coordinates = MoveRobot.convertAbsoluteToCoordinates(currentPos.left, currentPos.top);
         var coordX = coordinates[0];
         var coordY = coordinates[1];
@@ -191,6 +204,7 @@ var MoveRobot = {
             MoveRobot.place(coordX, coordY, direction);
         }
     },
+    //Place robot in the position provided and store the direction in it's attrib
     'place': function(x, y, direction) {
         var convertedUnits = MoveRobot.convertCoordinatesToAbsolute(x, y);
         if(MoveRobot.validMove(convertedUnits[0], convertedUnits[1]) && MoveRobot.validDirection(direction)) {
